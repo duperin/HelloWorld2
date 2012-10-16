57f5463dc57663fd07f36c125f2f4a554bcc4868
Eduardo Perin <du.perin@gmail.com>
2012-10-16 16:44:22 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Eduardo Perin on 10/16/12.
+//  Copyright (c) 2012 Eduardo Perin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Eduardo Perin on 10/16/12.
+//  Copyright (c) 2012 Eduardo Perin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
